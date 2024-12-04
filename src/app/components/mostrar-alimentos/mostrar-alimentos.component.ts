050245f967268dcac45d61bff6c655824d8a6c15
Gregorio Vélez Román <greverom4@gmail.com>
2024-12-04 16:55:11 -0500

    trabajando componente mostrar alimentos

--- src/app/components/mostrar-alimentos/mostrar-alimentos.component.ts
+++ src/app/components/mostrar-alimentos/mostrar-alimentos.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mostrar-alimentos',
   standalone: true,
-  imports: [],
+  imports: [
+      CommonModule,
+      FormsModule
+  ],
   templateUrl: './mostrar-alimentos.component.html',
   styleUrl: './mostrar-alimentos.component.css'
 })
 export class MostrarAlimentosComponent {
+  categorias: string[] = ['Entradas', 'Platos Fuertes', 'Postres', 'Bebidas'];
 
+  subcategoriasBebidas: { [key: string]: string[] } = {
+    'Bebidas Alcohólicas': ['Cervezas', 'Vinos', 'Whisky', 'Cocteles'],
+    'Bebidas No Alcohólicas': ['Colas', 'Jugos', 'Agua'],
+  };
+
+  selectedCategoria: string = '';
+  selectedSubcategoria: string = '';
+  subcategories: string[] = []; 
+
+  onCategoriaChange(): void {
+    if (this.selectedCategoria === 'Bebidas') {
+      this.subcategories = Object.keys(this.subcategoriasBebidas);
+    } else {
+      this.subcategories = [];
+      this.selectedSubcategoria = ''; 
+    }
+  }
+
+  onSubcategoriaChange(): void {
+    console.log('Subcategoría seleccionada:', this.selectedSubcategoria);
+  }
 }
